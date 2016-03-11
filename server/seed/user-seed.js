4f4561cd8ee55fe64cef8736e009d52205a6a7a1
Arun Varghese <avarghese@spireon.com>
2016-03-11 12:36:01 -0600

    remove user clear

--- server/seed/user-seed.js
+++ server/seed/user-seed.js
@@ -1,4 +1,3 @@
-Meteor.users.remove({});
 if (Meteor.users.find({}).count() === 0) {    	
 	var user = {username:"avarghese", password:"password", email:"arunv4700@gmail.com", roles:['admin']};
 	var id = Accounts.createUser(user);
