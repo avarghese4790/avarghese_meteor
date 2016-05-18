7043f1e3ef0104c7f0416b274466c33e27e439fb
Arun Varghese <avarghese@spireon.com>
2016-05-18 09:22:32 -0500

    styling on mobile

--- client/scripts/about.js
+++ client/scripts/about.js
@@ -1,11 +1,16 @@
 Template.about.onRendered(function(){
-	$('#aboutme-summary').typed({
-	    strings: ["I'm a full stack developer with a passion for design, coding, and creating tangible deliverables through problem solving and creative thinking!"],
-	    typeSpeed: 0,
-	    loop: false,
-	    showCursor: false,
-	    startDelay: 3000
-	});
+	var aboutmeSummary = "I'm a full stack developer with a passion for design, coding, and creating tangible deliverables through problem solving and creative thinking!";
+	if (Modernizr.touch){
+		$('#aboutme-summary').append(aboutmeSummary)	
+	} else {
+		$('#aboutme-summary').typed({
+		    strings: [aboutmeSummary],
+		    typeSpeed: 0,
+		    loop: false,
+		    showCursor: false,
+		    startDelay: 3000
+		});
+	}
 });
 
 Template.about.events({
